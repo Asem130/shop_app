25c8c69259d31a7435db6ec6a8a20d4ca8f18252
Asem130 <97855100+Asem130@users.noreply.github.com>
2023-12-15 00:37:59 +0200

    services

--- lib/services/category.dart
+++ lib/services/category.dart
@@ -1,10 +1,12 @@
 import 'package:shop_app/helper/api.dart';
 import 'package:shop_app/models/productes.dart';
 
-class GetAllProductesSevices {
-  Future<List<ProductesModel>> getAllProductes() async {
-    List<dynamic> data =
-        await Api().get(url: 'https://fakestoreapi.com/products');
+class CategoriesServices {
+  Future<List<ProductesModel>> getCategory(
+      {required String categoryName}) async {
+    List<dynamic> data = await Api()
+        .get(url: 'https://fakestoreapi.com/products/category/$categoryName');
+
     List<ProductesModel> getAllProductes = [];
     for (int i = 0; i < getAllProductes.length; i++) {
       getAllProductes.add(ProductesModel.fromJson(data[i]));
